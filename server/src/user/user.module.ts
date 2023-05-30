186b662d19fce061289023153973ed3aa8b65223
Junyeong Mun <junyeong.mun@corelinesoft.com>
2023-05-30 18:02:46 +0900

    access refresh token 발급

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -4,10 +4,18 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repo';
 import { User } from './entity/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      // TODO: 만약 env 활용하려면 cfg module, jwt module 추가해주기
+      secret: 'mysecret',
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService, UserRepository],
+  exports: [JwtModule],
 })
 export class UserModule {}
