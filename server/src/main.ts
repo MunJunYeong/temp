ffe30086d72b23eb356b35b819f2a8c47215ba12
hoon <csh3004@naver.com>
2023-06-01 13:29:22 +0900

    git branch test

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
 
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   // add filter
@@ -16,6 +17,7 @@ async function bootstrap() {
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     optionsSuccessStatus: 200,
+
   });
   app.useGlobalPipes(
     new ValidationPipe({
